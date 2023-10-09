112cbe1568dceccbd9eef5ddcdd6c8182ff14416
Joker-V2 <nipinna0@gmail.com>
2023-10-09 06:50:39 +0200

    violet: Fall back to oss sensors@1.0 HAL
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,8 @@ PRODUCT_PACKAGES += \
 
 # Sensor
 PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.0.vendor
 
 # Soong namespaces
